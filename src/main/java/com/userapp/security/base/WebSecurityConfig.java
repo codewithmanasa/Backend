11ea5e3e60563c38f9070a26cbe4f3c526ddda91
priyssri923 <chitra.codehub.java@gmail.com>
2026-02-22 16:45:38 +0530

    Updated WEbsecurity

--- src/main/java/com/userapp/security/base/WebSecurityConfig.java
+++ src/main/java/com/userapp/security/base/WebSecurityConfig.java
@@ -71,8 +71,8 @@ public class WebSecurityConfig {
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/auth/**").permitAll()
-                                .requestMatchers("/test/**").permitAll()
+                        auth.requestMatchers("api/auth/**").permitAll()
+                                .requestMatchers("api/test/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()));
